dc3766011c369572a6bafd4cf58c5f8bf3d92941
Dan May <danmay@gmail.com>
2015-04-03 19:43:48 -0400

    finish up problem 3

--- 3.py
+++ 3.py
@@ -1,8 +1,13 @@
+#!python
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
 
 class PrimeFactors():
 	def __init__(self, x):
 		self.x = x
-		self.pf = []
 		self.pfp = 1
 
 	def __iter__(self):
@@ -11,6 +16,16 @@ class PrimeFactors():
 	def __next__(self):
 		y = self.x / self.pfp
 
+		if y == 1:
+			raise StopIteration()
+
 		#find the next factor of y. the least factor of a composite must be prime
 		i = 2
-		while(i<sqrt(y))
+		while(i<=y):
+			if y%i == 0:
+				self.pfp = self.pfp * i
+				return i
+			i = i+1
+
+print(max(PrimeFactors(600851475143)))
+
