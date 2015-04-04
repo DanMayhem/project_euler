f658a97f25204ef292e46cb300f6acef8e4c98cb
Dan May <danmay@gmail.com>
2015-04-03 20:52:52 -0400

    solve 4

--- 4.py
+++ 4.py
@@ -4,3 +4,15 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+
+def is_palindrome(i):
+	return i == int(str(i)[::-1])
+
+p = 1
+
+for i in range(100,1000):
+	for j in range(100,1000):
+		if is_palindrome(i*j) and (i*j) > p:
+			p = i*j
+
+print(p)
